12a953e5416d75111c50ad1cba8f2ac57f7c5d29
Gurar <jozyk@inbox.lt>
2020-11-11 15:48:23 +0200

    Js starting

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
-console.log("hello");
+const vardas = 'Vardenis';
+const amzius = 99;
+
+console.log(amzius);
+console.log(vardas);
+
+// Vardenis yra 99 metu azius.
+
+const sakinys = vardas + ' yra ' + amzius + ' metu aziaus.';
+
+console.log(sakinys);
+
+const suo = 'Rikis';
+const maistas = 'kaulas';
+
+// Rikis megstamas maistas yra kaulas.
+
+const sakinys2 = suo + ' megstamas maistas yra ' + maistas + '.';
+console.log(sakinys2);
+
+const heroName = 'Chuck';
+const heroLastname = 'Norris';
+const currentYear = 2020;
+const birth = 1940;
+const age = currentYear - birth;
+
+// Chuck Norris (80);
+
+const result = heroName + ' ' + heroLastname + ' (' + age + ')';
+console.log(result);
+
+let pinigine = 0;
+const sausis = 100;
+const vasaris = 200;
+const kovas = 70;
+
+// 100
+// 200
+// 370
+
+pinigine = pinigine + sausis;
+console.log(pinigine);
+
+pinigine = pinigine + vasaris;
+console.log(pinigine);
+
+pinigine = pinigine + kovas;
+console.log(pinigine);
