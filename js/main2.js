fe2913fe280d42c1af8249294debfec62142a125
Gurar <jozyk@inbox.lt>
2020-11-11 16:18:27 +0200

    tekstai

---
+++ js/main2.js
@@ -0,0 +1,33 @@
+const vardas = 'Vardenis';
+const pasisveikinima = "Labas rytas, Lietuva!";
+
+//do not > dont't
+
+const neiginys = "don't";
+console.log(neiginys);
+
+//Jis pasake: "Labas".
+
+const fraze = 'Jis pasake: "Labas".';
+console.log(fraze);
+
+//Mother said: "Don't do it!".
+const mother1 = "Mother said: \"Don't do it!\".";
+const mother2 = 'Mother said: "Don\'t do it!".';
+
+console.log(mother1);
+console.log(mother2);
+
+/* 
+<div>
+    <p>lorem upsum its amet</p>
+    <a href="#"> Don't Click me</a>
+</div>
+*/
+
+const html = `<div>
+    <p>Lorem upsum it's amet</p>
+    <a href="#">Don't Click me</a>
+</div>`;
+
+console.log(html);
